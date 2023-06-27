8f73ba99f72b2735760b47e6e337faf3f648ae1d
Andreas Tzionis <andy0n7@gmail.com>
2023-06-27 03:38:59 +0300

    cleanup

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useRef } from 'react'
 
 import { useCurrentFrame, interpolate, useVideoConfig } from 'remotion'
 
-import type { NonDeletedExcalidrawElement, } from '@excalidraw/excalidraw/types/element/types'
 import type { ExportedDataState, } from '@excalidraw/excalidraw/types/data/types'
 
 import { exportToSvg, } from '@excalidraw/excalidraw'
@@ -18,27 +17,28 @@ const Canvas: React.FC = () => {
 
   const frame = useCurrentFrame()
   const { durationInFrames } = useVideoConfig()
-  const opacity = interpolate(frame, [0 , durationInFrames], [0, 1])
+  const progress = interpolate(frame, [0 , durationInFrames], [0, 1])
 
   useEffect(() => {
     (async () => {
+
+      // load
       const data = example as ExportedDataState
-      const elements = data.elements.filter((e): e is NonDeletedExcalidrawElement => !e.isDeleted)
+      const elements = data.elements.filter((e) => !e.isDeleted)
       const exportOptions = { elements, files: data.files!, appState: data.appState, exportPadding: 30, }
       const svg = await exportToSvg(exportOptions)
-      animateSvg(svg, elements, {})
       setSvg(svg)
-    })()
-  }, [])
 
-  useEffect(() => {
-    if (svg) {
+      // append
       svg.style.width = '100%'
       svg.style.height = '100%'
       ref.current?.appendChild(svg)
-    }
-    return () => svg?.remove()
-  }, [svg])
+
+      // animate
+      animateSvg(svg, elements, {})
+
+    })()
+  }, [])
 
   const toggleAnimation = () => {
     if (!paused) svg?.pauseAnimations()
@@ -54,7 +54,7 @@ const Canvas: React.FC = () => {
 
   return (
     <div>
-      <h1>{opacity.toFixed(2)}</h1>
+      <h1>{progress.toFixed(2)}</h1>
       <div>
         <button type="button" onClick={() => toggleAnimation()}>{paused ? 'Play' : 'Pause'}</button>
         <button type="button" onClick={() => stepAnimation()}>Step</button>
